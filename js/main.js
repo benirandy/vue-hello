7b6ed2b291fc1e7608ab63118f552a60a40daef2
Beni Randy Harold <haroldbeni111997@gmail.com>
2023-03-14 19:55:32 +0100

    finito

--- js/main.js
+++ js/main.js
@@ -4,11 +4,11 @@ createApp({
     data() {
         return {
             message: 'Benvenuto in Vue.js!',
-           image: 'https://vuejs.org/images/logo.png'
+            image: 'https://vuejs.org/images/logo.png'
         }
     }
 }).mount('#app')
 
 
-
+/*La directive v-bind permette di  stampare un link dentro ad un attributo html in modo dinamico (ovvero generato da Vue).*/
 
