d6618674aa19f1440c2c21a18286da26e184609d
Beni Randy Harold <haroldbeni111997@gmail.com>
2023-03-14 19:45:51 +0100

    progetto finito

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            message: 'Benvenuto in Vue.js!',
+           image: 'https://vuejs.org/images/logo.png'
+        }
+    }
+}).mount('#app')
+
+
+
+
